de3f71e98ae499ac30e932e7d08fe771131ae252
leijiangtao <2509934810@qq.com>
2020-08-07 17:30:38 +0800

    reset vuatual

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ from setuptools import setup, find_packages
 setup(
     name="vuatual",
     version="1.0.0",
-    install_requires=["pytest==5.4.3", "click==7.1.2"],
+    install_requires=[
+        "pytest==5.4.1", 
+        "click==7.1.2",
+        "selenium==3.141.0",
+        "PyMySQL==0.10.0",
+        "requests==2.24.0",
+        "pytest-parallel==0.1.0",
+        ],
     packages=find_packages(),
     include_package_data=True,
     entry_points={
